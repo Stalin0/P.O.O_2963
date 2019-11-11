7fe824afbf2dfcee4b66194cb1f3ca76cb553002
STALIN RIVERA VEGA <55595831+Stalin0@users.noreply.github.com>
2019-11-11 01:07:43 -0500

    Correction

---
+++ Assignment/Corretion Book/Book/src/book/BookManager.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package book;
+
+import ec.edu.espe.Book;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Usuario
+ */
+class BookManager {
+
+    ArrayList<Book> readAllBooks() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void addNewBook(ArrayList<Book> arrBook) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void showBookByEditorial(String editorial) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void showBookByYear(int year) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void countBookByEditorial(String editorialcount) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
